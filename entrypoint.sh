b25063760574276ae21ab13d186916a2b1dbaef8
Damian Sloane <damian@capacitorlabs.com>
2020-10-22 19:13:04 +1100

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,7 @@ if [ -f "devops.tf" ]; then
   echo
   echo
   echo "Running Terraform apply"
+  export
   terraform apply -auto-approve ${INPUT_APPLY_PARAMETERS} || exit 4
 else
   # Failed to locate Terraform backend definition
